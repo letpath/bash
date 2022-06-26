0bdb92dec177646c30b0f3d70c1c78e55c67c186
tom-sapletta-com <tom@sapletta.com>
2022-06-26 15:11:42 +0200

    update for functions

--- list.sh
+++ list.sh
@@ -8,10 +8,10 @@
 # cat index.html | ./tag.sh title
 
 # START
-[ -t 0 ] && echo "stdin File Content is empty" && exit
-IFS='' read -d '' -r PATHH
+[ ! -t 0 ] && IFS='' read -d '' -r PATHH
 [ -z "$PATHH" ] && PATHH=$1
 [ ! -z "$1" ] && OPTIONS=$1
-PATH_FILE="./${FILENAME}"
-[ -z "$OPTIONS" ] && ls $PATH_FILE
-[ ! -z "$OPTIONS" ] && ls $OPTIONS $PATH_FILE
+#PATH_FILE="./${FILENAME}"
+[ -z "$OPTIONS" ] && ls
+#[ -z "$OPTIONS" ] && ls $PATH_FILE
+#[ ! -z "$OPTIONS" ] && ls $OPTIONS $PATH_FILE
