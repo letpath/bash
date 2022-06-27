2c18b4c594b1d460938fbad2f4313709e8aabb08
tom-sapletta-com <tom@sapletta.com>
2022-06-27 14:40:09 +0200

    test function

--- createFile.sh
+++ createFile.sh
@@ -2,6 +2,7 @@
 FILENAME=$1
 CONTENT=
 [ ! -z "$FILENAME" ] && IFS='' read -d '' -r CONTENT
+[ -z "$CONTENT" ] &&  touch "${FILENAME}"
 [ -z "$FILENAME" ] && IFS='' read -d '' -r FILENAME
 [ ! -z "$FILENAME" ] && echo -n "$CONTENT" > $FILENAME
 
