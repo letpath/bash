1802a872183c94713a6e15b790b46eefe73c883b
tom-sapletta-com <tom@sapletta.com>
2022-06-27 11:47:48 +0200

    wthout next line

--- appendToFile.sh
+++ appendToFile.sh
@@ -7,4 +7,4 @@ PATH_FILE="${FILENAME}"
 ( [ -e "${PATH_FILE}" ] || touch "${PATH_FILE}" ) && [ ! -w "${PATH_FILE}" ] && echo "CONTENT can not be written to FILE: ${PATH_FILE}" && exit
 #echo "${CONTENT}"
 #echo "${PATH_FILE}"
-echo "${CONTENT}" >> "${PATH_FILE}"
+echo -n "${CONTENT}" >> "${PATH_FILE}"
