293701dc6d107c8cd677ada44482768fc7de4aef
tom-sapletta-com <tom@sapletta.com>
2022-07-22 21:02:41 +0200

    test function for append content from file

--- appendCellFromFile.sh
+++ appendCellFromFile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-[ ! -t 0 ] && IFS='' read -d '' -r CONTENT && echo -n "${CONTENT}"
+[ ! -t 0 ] && IFS='' read -d '' -r CONTENT
+echo -n "${CONTENT}"
 [ -z "$1" ] && echo "FILENAME is empty" && exit
 FILENAME=$1
 [ ! -z "$FILENAME" ] && echo -n "," && cat $FILENAME
