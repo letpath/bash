67d646a110194636a858ad727dadab8b708c84dd
tom-sapletta-com <tom@sapletta.com>
2022-07-22 20:52:55 +0200

    test function for append content from file

--- appendContentFromFile.sh
+++ appendContentFromFile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-[ -t 0 ] && echo "stdin File Content is empty" && exit
-IFS='' read -d '' -r CONTENT
-echo -n "${CONTENT}"
+[ ! -t 0 ] && IFS='' read -d '' -r CONTENT && echo -n "${CONTENT}"
 [ -z "$1" ] && echo "FILENAME is empty" && exit
 FILENAME=$1
 [ ! -z "$FILENAME" ] && cat $FILENAME
